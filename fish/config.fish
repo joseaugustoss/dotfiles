a612028880e572f6bbacd4afa654b3a090d3886a
José Augusto <joseaugusto0408@gmail.com>
2025-09-20 07:04:38 -0300

    atualizaçao do zshrc e adicionando fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+starship init fish | source
+
 if status is-interactive
     # onde o asdf guarda os plugins e shims
     set -x ASDF_DATA_DIR $HOME/.local/share/asdf
@@ -19,9 +21,14 @@ if status is-interactive
     set -Ux PATH $PATH $ANDROID_HOME/tools/bin
     set -Ux PATH $PATH $ANDROID_HOME/platform-tools
 
+    set -Ux PATH $HOME/.local/bin $PATH
     function v
         nvim $argv
     end
+
+    abbr ytmp3 'yt-dlp -x --audio-format mp3 -o "%(title)s.%(ext)s"'
+    abbr ytmp4 'yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4" -o "%(title)s.%(ext)s"'
+    abbr ytplmp3 'yt-dlp -x --audio-format mp3 -o "%(playlist_index)02d - %(title)s.%(ext)s"'
 
     ## alias 
     alias ls='colorls'
